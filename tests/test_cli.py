430ce90d535cf7cb17749875a61b0edebc8a6f9c
Emil Sadek <esadek@users.noreply.github.com>
2025-02-17 21:20:22 -0800

    Change: test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,14 +1,21 @@
 import unittest
+from unittest.mock import MagicMock, mock_open, patch
 
 from tsks.cli import get_available_tasks
 
 
-class TestCli(unittest.TestCase):
-    def test_get_available_tasks(self) -> None:
-        available_tasks = get_available_tasks()
-        self.assertEqual(available_tasks.get("test"), "uv run -m unittest")
-        self.assertEqual(available_tasks.get("lint"), "uv run ruff check --fix")
-        self.assertEqual(available_tasks.get("format"), "uv run ruff format")
+class TestGetAvailableTasks(unittest.TestCase):
+    @patch(
+        "builtins.open", new_callable=mock_open, read_data="[tool.tsks]\nkey = 'value'"
+    )
+    @patch("tomllib.load")
+    def test_valid_file(self, mock_load: MagicMock, mock_file: MagicMock) -> None:
+        expected_output = {"key": "value"}
+        mock_load.return_value = {"tool": {"tsks": expected_output}}
+        result = get_available_tasks()
+        self.assertEqual(result, expected_output)
+        mock_file.assert_called_once_with("pyproject.toml", "rb")
+        mock_load.assert_called_once_with(mock_file.return_value)
 
 
 if __name__ == "__main__":

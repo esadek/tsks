681a22206b2abf68dc352fb61363e66481240d21
Emil Sadek <esadek@users.noreply.github.com>
2025-02-17 20:09:50 -0800

    Add: argument type

--- src/tsks/cli.py
+++ src/tsks/cli.py
@@ -45,6 +45,6 @@ def cli() -> None:
     parser = argparse.ArgumentParser(
         prog="tsks", description="simple task runner for python projects"
     )
-    parser.add_argument("tasks", nargs="+")
+    parser.add_argument("tasks", type=str, nargs="+")
     tasks: list[str] = parser.parse_args().tasks
     run_tasks(tasks)
